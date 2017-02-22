aecba15a4698e4554b781672b384444cda2322d6
Adrián <info@adrianmelic.com>
2017-02-22 19:29:09 +0100

    Mega commit for 1_lungs_roi_arnav with Adrián and Alberto changes

--- src/1_lungs_roi_arnav.py
+++ src/1_lungs_roi_arnav.py
@@ -1,29 +1,20 @@
-# Basado en:
+# Based on:
 # https://www.kaggle.com/arnavkj95/data-science-bowl-2017/candidate-generation-and-luna16-preprocessing/notebook
 
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
-
 import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import skimage, os
-from skimage.morphology import ball, disk, dilation, binary_erosion, remove_small_objects, erosion, closing, reconstruction, binary_closing
-from skimage.measure import label,regionprops, perimeter
-from skimage.morphology import binary_dilation, binary_opening
-from skimage.filters import roberts, sobel
-from skimage import measure, feature
+import os
+from skimage.morphology import disk, binary_erosion, binary_closing
+from skimage.measure import label,regionprops
+from skimage.filters import roberts
 from skimage.segmentation import clear_border
-from skimage import data
 from scipy import ndimage as ndi
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import dicom
-import scipy.misc
-import numpy as np
+import pickle
 
 import sys
 sys.path.append("../")
+# Import our competition variables
 from competition_config import *
 d=arnavs_lugns_roi_dashboard
 
@@ -34,149 +25,177 @@ INPUT_DIRECTORY = d['INPUT_DIRECTORY']
 OUTPUT_DIRECTORY = d['OUTPUT_DIRECTORY']
 
 if not os.path.exists(OUTPUT_DIRECTORY):
-    os.makedirs(OUTPUT_DIRECTORY)
+	os.makedirs(OUTPUT_DIRECTORY)
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+# Input data files are available in the INPUT_DIRECTORY.
+# Any results you write has to be saved in the OUTPUT_DIRECTORY.
 
-from subprocess import check_output
-print(check_output(["ls", INPUT_DIRECTORY]).decode("utf8"))
-
-ct_scan_id='00cba091fa4ad62cc3200a657aeb957e'
-
-# Any results you write to the current directory are saved as output.
-lung = dicom.read_file(INPUT_DIRECTORY + ct_scan_id + '/38c4ff5d36b5a6b6dc025435d62a143d.dcm')
-
-slice = lung.pixel_array
-slice[slice == -2000] = 0
-plt.imshow(slice, cmap=plt.cm.gray)
 
 def read_ct_scan(folder_name):
-        # Read the slices from the dicom file
-        slices = [dicom.read_file(folder_name + filename) for filename in os.listdir(folder_name)]
-        
-        # Sort the dicom slices in their respective order
-        slices.sort(key=lambda x: int(x.InstanceNumber))
-        
-        # Get the pixel values for all the slices
-        slices = np.stack([s.pixel_array for s in slices])
-        slices[slices == -2000] = 0
-        return slices
+	# Read the slices from the dicom file
+	slicesL = [dicom.read_file(folder_name + filename) for filename in os.listdir(folder_name)]
+	
+	# Sort the dicom slices in their respective order
+	slicesL.sort(key=lambda x: int(x.InstanceNumber))
+	
+	slice_thickness = slicesL[0].ImagePositionPatient[2] - \
+	slicesL[1].ImagePositionPatient[2] # Will be < 0 on inverted images
+		
+	#slice_thickness1 = slicesL[0].SliceLocation - slicesL[1].SliceLocation
+	#print(folder_name, slice_thickness, slice_thickness1)
+		
+	# Get the pixel values for all the slices
+	slicesA = np.stack([s.pixel_array for s in slicesL])
+	slicesA = slicesA.astype(np.int16)
+	
+	# Convert to Hounsfield units (HU)
+	intercept = slicesL[0].RescaleIntercept
+	slope = slicesL[0].RescaleSlope
+	
+	if slope != 1:
+		slicesA = slope * slicesA.astype(np.float64)
+		slicesA = slicesA.astype(np.int16)
 
-ct_scan = read_ct_scan(INPUT_DIRECTORY + ct_scan_id + '/') 
+	slicesA += np.int16(intercept)
+	
+	# Saw -2048 instead of -2000, original has == -2000, changed for <= -2000
+	# Instead of 0 (water), we use -1000 (air)
+	slicesA[slicesA <= -2000] = -1000
+	slicesA[slicesA <= -2000] = -1000
 
-#Now we will plot a few more images of the slices using the plot_ct_scan function.
+	slicesA += 1000 # So it will show
+	
+	# For inverted lungs:
+	if slice_thickness < 0:
+		slicesA = slicesA[::-1]
+
+	slicesA = np.array(slicesA, dtype=np.int16)
+
+	return slicesA
+
+
+# Now we will plot a few more images of the slices using the plot_ct_scan function.
 
 def plot_ct_scan(scan):
-    f, plots = plt.subplots(int(scan.shape[0] / 20) + 1, 4, figsize=(25, 25))
-    for i in range(0, scan.shape[0], 5):
-        plots[int(i / 20), int((i % 20) / 5)].axis('off')
-        plots[int(i / 20), int((i % 20) / 5)].imshow(scan[i], cmap=plt.cm.bone) 
+	f, plots = plt.subplots(int(scan.shape[0] / 20) + 1, 4, figsize=(25, 25))
+	for i in range(0, scan.shape[0], 5):
+		plots[int(i / 20), int((i % 20) / 5)].axis('off')
+		plots[int(i / 20), int((i % 20) / 5)].imshow(scan[i], cmap=plt.cm.bone) 
 
-plot_ct_scan(ct_scan)
+
+# This funtion segments the lungs from the given 2D slice.
 
 def get_segmented_lungs(im, plot=False):
-    
-    '''
-    This funtion segments the lungs from the given 2D slice.
-    '''
-    if plot == True:
-        f, plots = plt.subplots(8, 1, figsize=(5, 40))
-    '''
-    Step 1: Convert into a binary image. 
-    '''
-    binary = im < 604
-    if plot == True:
-        plots[0].axis('off')
-        plots[0].imshow(binary, cmap=plt.cm.bone) 
-    '''
-    Step 2: Remove the blobs connected to the border of the image.
-    '''
-    cleared = clear_border(binary)
-    if plot == True:
-        plots[1].axis('off')
-        plots[1].imshow(cleared, cmap=plt.cm.bone) 
-    '''
-    Step 3: Label the image.
-    '''
-    label_image = label(cleared)
-    if plot == True:
-        plots[2].axis('off')
-        plots[2].imshow(label_image, cmap=plt.cm.bone) 
-    '''
-    Step 4: Keep the labels with 2 largest areas.
-    '''
-    areas = [r.area for r in regionprops(label_image)]
-    areas.sort()
-    if len(areas) > 2:
-        for region in regionprops(label_image):
-            if region.area < areas[-2]:
-                for coordinates in region.coords:                
-                       label_image[coordinates[0], coordinates[1]] = 0
-    binary = label_image > 0
-    if plot == True:
-        plots[3].axis('off')
-        plots[3].imshow(binary, cmap=plt.cm.bone) 
-    '''
-    Step 5: Erosion operation with a disk of radius 2. This operation is 
-    seperate the lung nodules attached to the blood vessels.
-    '''
-    selem = disk(2)
-    binary = binary_erosion(binary, selem)
-    if plot == True:
-        plots[4].axis('off')
-        plots[4].imshow(binary, cmap=plt.cm.bone) 
-    '''
-    Step 6: Closure operation with a disk of radius 10. This operation is 
-    to keep nodules attached to the lung wall.
-    '''
-    selem = disk(10)
-    binary = binary_closing(binary, selem)
-    if plot == True:
-        plots[5].axis('off')
-        plots[5].imshow(binary, cmap=plt.cm.bone) 
-    '''
-    Step 7: Fill in the small holes inside the binary mask of lungs.
-    '''
-    edges = roberts(binary)
-    binary = ndi.binary_fill_holes(edges)
-    if plot == True:
-        plots[6].axis('off')
-        plots[6].imshow(binary, cmap=plt.cm.bone) 
-    '''
-    Step 8: Superimpose the binary mask on the input image.
-    '''
-    get_high_vals = binary == 0
-    im[get_high_vals] = 0
-    if plot == True:
-        plots[7].axis('off')
-        plots[7].imshow(im, cmap=plt.cm.bone) 
-        
-    return im
+	
+	if plot == True:
+		f, plots = plt.subplots(8, 1, figsize=(5, 40))
 
-get_segmented_lungs(ct_scan[71], True)
+	# Step 1: Convert into a binary image.
+	# A threshold of 604(-400 HU) is used at all places because it was
+	# found in experiments that it works just fine
+	binary = im < 604
+	if plot == True:
+		print('Step 1: Convert into a binary image.')
+		plots[0].axis('off')
+		plots[0].imshow(binary, cmap=plt.cm.bone) 
 
+	# Step 2: Remove the blobs connected to the border of the image.
+	cleared = clear_border(binary)
+	if plot == True:
+		print('Step 2: Remove the blobs connected to the border of the image.')
+		plots[1].axis('off')
+		plots[1].imshow(cleared, cmap=plt.cm.bone) 
+	
+	# Step 3: Label the image.
+	label_image = label(cleared)
+	if plot == True:
+		print('Step 3: Label the image.')
+		plots[2].axis('off')
+		plots[2].imshow(label_image, cmap=plt.cm.bone) 
+
+	# Step 4: Keep the labels with 2 largest areas.
+	areas = [r.area for r in regionprops(label_image)]
+	areas.sort()
+	if len(areas) > 2:
+		for region in regionprops(label_image):
+			if region.area < areas[-2]:
+				for coordinates in region.coords:
+					label_image[coordinates[0], coordinates[1]] = 0
+	binary = label_image > 0
+	if plot == True:
+		print('Step 4: Keep the labels with 2 largest areas.')
+		plots[3].axis('off')
+		plots[3].imshow(binary, cmap=plt.cm.bone) 
+
+	# Step 5: Erosion operation with a disk of radius 2. This operation is 
+	# seperate the lung nodules attached to the blood vessels
+	# using EROSION_BALL_RADIUS from competition_config.py
+	selem = disk(arnavs_lugns_roi_dashboard['EROSION_BALL_RADIUS'])
+	binary = binary_erosion(binary, selem)
+	if plot == True:
+		print('Step 5: Erosion operation')
+		plots[4].axis('off')
+		plots[4].imshow(binary, cmap=plt.cm.bone) 
+	
+	# Step 6: Closure operation with a disk of radius 10. This operation is 
+	# to keep nodules attached to the lung wall
+	# using CLOSING_BALL_RADIUS from competition_config.py
+	selem = disk(arnavs_lugns_roi_dashboard['CLOSING_BALL_RADIUS'])
+	binary = binary_closing(binary, selem)
+	if plot == True:
+		print('Step 6: Closure operation')
+		plots[5].axis('off')
+		plots[5].imshow(binary, cmap=plt.cm.bone) 
+
+	# Step 7: Fill in the small holes inside the binary mask of lungs.
+	edges = roberts(binary)
+	binary = ndi.binary_fill_holes(edges)
+	if plot == True:
+		print('Step 7: Fill in the small holes inside the binary mask of lungs.')
+		plots[6].axis('off')
+		plots[6].imshow(binary, cmap=plt.cm.bone) 
+
+	# Step 8: Superimpose the binary mask on the input image.
+	get_high_vals = binary == 0
+	im[get_high_vals] = 0
+	if plot == True:
+		print('Step 8: Superimpose the binary mask on the input image.')
+		plots[7].axis('off')
+		plots[7].imshow(im, cmap=plt.cm.bone) 
+		
+	return im
 
 
 def segment_lung_from_ct_scan(ct_scan):
-    return np.asarray([get_segmented_lungs(slice) for slice in ct_scan])
+	return np.asarray([get_segmented_lungs(slice) for slice in ct_scan])
 
 
+def segment_all_ct_scans(path): # Iterate through all folders
+	all_folders = os.listdir(path)
+	print('Found ' + str(len(all_folders)) + ' scans. Segmenting...')
+	i = 0
+	for folder in all_folders:
+		i += 1
+		i_start_time = time.time()
+		# If pickle already exist, skip to the next folder
+		if os.path.isfile(OUTPUT_DIRECTORY + folder + '.pickle'):
+			print(folder + ' already segmented. Skipping...')
+			continue
+	
+		# Segment
+		ct_scan = read_ct_scan(path + folder + '/') 
+		segmented_ct_scan = segment_lung_from_ct_scan(ct_scan)
+		
+		# Save object as a .pickle
+		with open(OUTPUT_DIRECTORY + folder + ".pickle", 'wb') as handle:
+			pickle.dump(segmented_ct_scan, handle, protocol=pickle.HIGHEST_PROTOCOL)
+		
+		# Print and time to finish
+		time_finish = round(time.time() - i_start_time) * (len(all_folders) - i)
+		print(folder + ' segmentation created. About ' + \
+			str(time.strftime('%H:%M:%S', time.gmtime(time_finish))) + \
+			' left.')
 
-segmented_ct_scan = segment_lung_from_ct_scan(ct_scan)
-plot_ct_scan(segmented_ct_scan)
+segment_all_ct_scans(INPUT_DIRECTORY)
 
-import pickle
-with open(OUTPUT_DIRECTORY + ct_scan_id + ".pickle", 'wb') as handle:
-    pickle.dump(segmented_ct_scan, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-print("Ellapsed time: {} seconds".format((time.time() - start_time)))
-
-
-
-
-
-
-
-
-
+print("Total elapsed time: {} seconds".format(round((time.time() - start_time), 2)))
