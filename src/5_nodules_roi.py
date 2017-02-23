f2074c20bf45f3b71bc3050b9338865b91329b16
Adrián <info@adrianmelic.com>
2017-02-23 21:55:31 +0100

    Mega commit for 5_nodules_roi with Adrián's changes

--- src/5_nodules_roi.py
+++ src/5_nodules_roi.py
@@ -1,30 +1,19 @@
-# Basado en:
+# Based on:
 # https://www.kaggle.com/arnavkj95/data-science-bowl-2017/candidate-generation-and-luna16-preprocessing/notebook
 
-# This Python 3 environment comes with many helpful analytics libraries installed
-# It is defined by the kaggle/python docker image: https://github.com/kaggle/docker-python
-# For example, here's several helpful packages to load in 
 
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import skimage, os
-from skimage.morphology import ball, disk, dilation, binary_erosion, remove_small_objects, erosion, closing, reconstruction, binary_closing
-from skimage.measure import label,regionprops, perimeter
-from skimage.morphology import binary_dilation, binary_opening
-from skimage.filters import roberts, sobel
-from skimage import measure, feature
-from skimage.segmentation import clear_border
-from skimage import data
-from scipy import ndimage as ndi
+import os
+from skimage.morphology import ball, binary_closing
+from skimage.measure import label,regionprops
+from skimage import measure
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 import dicom
-import scipy.misc
-import numpy as np
 import pickle
 
 import sys
 sys.path.append("../")
+# Import our competition variables
 from competition_config import *
 d=nodules_roi_dashboard
 
@@ -37,22 +26,16 @@ OUTPUT_DIRECTORY = d['OUTPUT_DIRECTORY']
 if not os.path.exists(OUTPUT_DIRECTORY):
     os.makedirs(OUTPUT_DIRECTORY)
 
-# Input data files are available in the "../input/" directory.
-# For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
+# Input data files are available in the INPUT_DIRECTORY.
+# Any results you write has to be saved in the OUTPUT_DIRECTORY.
 
-from subprocess import check_output
-print(check_output(["ls", INPUT_DIRECTORY]).decode("utf8"))
-
-ct_scan_id='00cba091fa4ad62cc3200a657aeb957e'
-
-with open(INPUT_DIRECTORY + ct_scan_id + '.pickle', 'rb') as handle:
-    segmented_ct_scan = pickle.load(handle)
 
 def plot_ct_scan(scan):
     f, plots = plt.subplots(int(scan.shape[0] / 20) + 1, 4, figsize=(25, 25))
     for i in range(0, scan.shape[0], 5):
         plots[int(i / 20), int((i % 20) / 5)].axis('off')
         plots[int(i / 20), int((i % 20) / 5)].imshow(scan[i], cmap=plt.cm.bone) 
+
 
 def plot_3d(image, threshold=-300):
     
@@ -79,56 +62,87 @@ def plot_3d(image, threshold=-300):
     plt.show()
     
 
-segmented_ct_scan[segmented_ct_scan < 604] = 0
-plot_ct_scan(segmented_ct_scan)
+def get_nodules(segmented_ct_scan):
+	
+	segmented_ct_scan[segmented_ct_scan < 604] = 0
+	#plot_ct_scan(segmented_ct_scan)
+	#plot_3d(segmented_ct_scan, 604)
 
-plot_3d(segmented_ct_scan, 604)
+	# After filtering, there are still lot of noise because of blood vessels.
+	# Thus we further remove the two largest connected component.
 
-# After filtering, there are still lot of noise because of blood vessels. Thus we further remove the two largest connected component.
+	selem = ball(d['BALL_RADIUS'])
+	binary = binary_closing(segmented_ct_scan, selem)
 
-selem = ball(d['BALL_RADIUS'])
-binary = binary_closing(segmented_ct_scan, selem)
+	label_scan = label(binary)
 
-label_scan = label(binary)
+	areas = [r.area for r in regionprops(label_scan)]
+	areas.sort()
 
-areas = [r.area for r in regionprops(label_scan)]
-areas.sort()
+	for r in regionprops(label_scan):
+	    max_x, max_y, max_z = 0, 0, 0
+	    min_x, min_y, min_z = 1000, 1000, 1000
+	    
+	    for c in r.coords:
+	        max_z = max(c[0], max_z)
+	        max_y = max(c[1], max_y)
+	        max_x = max(c[2], max_x)
+	        
+	        min_z = min(c[0], min_z)
+	        min_y = min(c[1], min_y)
+	        min_x = min(c[2], min_x)
+	    if (min_z == max_z or min_y == max_y or min_x == max_x or r.area > areas[-3]):
+	        for c in r.coords:
+	            segmented_ct_scan[c[0], c[1], c[2]] = 0
+	    else:
+	        index = (max((max_x - min_x), (max_y - min_y), (max_z - min_z))) / \
+				 (min((max_x - min_x), (max_y - min_y) , (max_z - min_z)))
+	
+	'''Comment from ArnavJain on Kaggle about index: 
+	Index is the shape index of the 3D blob. As mentioned in the TODOs, 
+	I am working on reducing the generated candidates using shape index 
+	and other properties. So, index is one such property.
+	I will post the whole part as soon as I complete it.'''
+			
+	return segmented_ct_scan
 
-for r in regionprops(label_scan):
-    max_x, max_y, max_z = 0, 0, 0
-    min_x, min_y, min_z = 1000, 1000, 1000
-    
-    for c in r.coords:
-        max_z = max(c[0], max_z)
-        max_y = max(c[1], max_y)
-        max_x = max(c[2], max_x)
-        
-        min_z = min(c[0], min_z)
-        min_y = min(c[1], min_y)
-        min_x = min(c[2], min_x)
-    if (min_z == max_z or min_y == max_y or min_x == max_x or r.area > areas[-3]):
-        for c in r.coords:
-            segmented_ct_scan[c[0], c[1], c[2]] = 0
-    else:
-        index = (max((max_x - min_x), (max_y - min_y), (max_z - min_z))) / (min((max_x - min_x), (max_y - min_y) , (max_z - min_z)))
+		
+def get_nodules_all_segmented_ct_scans(path, image=False): # Iterate through all folders
+	
+	# Read all segmented lungs pickles, ignore other extension files (like images)
+	all_pickles = [p.split('.')[0] for p in os.listdir(path) if p.endswith('.pickle')]
+	print('Found ' + str(len(all_pickles)) + ' segmented lungs. Getting nodules...')
+	
+	i = 0
+	for segmented_lung in all_pickles:
+		i += 1
+		i_start_time = time.time()
+		# If pickle already exist, skip to the next folder
+		if os.path.isfile(OUTPUT_DIRECTORY + segmented_lung + '.pickle'):
+			print(segmented_lung + ' already segmented. Skipping...')
+			continue
+		# Load pickle with segmented_ct_scan
+		with open(path + segmented_lung + '.pickle', 'rb') as handle:
+			segmented_ct_scan = pickle.load(handle)
+		
+		# Get nodules
+		segmented_nodules_ct_scan = get_nodules(segmented_ct_scan)
+		
+		# Save image as .png of 3D plotted segmented nodules
+		if image==True:
+			plot_3d(segmented_nodules_ct_scan, 604)
+			plt.savefig(OUTPUT_DIRECTORY + segmented_lung + '.png', format='png')
+		
+		# Save object as a .pickle
+		with open(OUTPUT_DIRECTORY + segmented_lung + ".pickle", 'wb') as handle:
+			pickle.dump(segmented_nodules_ct_scan, handle, protocol=pickle.HIGHEST_PROTOCOL)	
+		
+		# Print and time to finish
+		time_finish = round(time.time() - i_start_time) * (len(all_pickles) - i)
+		print(segmented_lung + 'nodules segmentation created. About ' + \
+			str(time.strftime('%H:%M:%S', time.gmtime(time_finish))) + \
+			' left.')
 
+get_nodules_all_segmented_ct_scans(INPUT_DIRECTORY, True)
 
-####################################################
-
-
-plot_3d(segmented_ct_scan, 604)
-
-import pickle
-with open(OUTPUT_DIRECTORY + ct_scan_id + ".pickle", 'wb') as handle:
-    pickle.dump(segmented_ct_scan, handle, protocol=pickle.HIGHEST_PROTOCOL)
-
-print("Ellapsed time: {} seconds".format((time.time() - start_time)))
-
-
-
-
-
-
-
-
-
+print("Elapsed time: {} seconds".format((time.time() - start_time)))
