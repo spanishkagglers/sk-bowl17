a57c7160df0d4a43dc3f07999a841b86fda14b92
Adrián <info@adrianmelic.com>
2017-03-05 04:10:24 +0100

    Esthetics

--- src/5_nodules_roi.py
+++ src/5_nodules_roi.py
@@ -1,49 +1,52 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  3 00:59:56 2017
+
+@author: spanish kagglers
+"""
 # Based on:
 # https://www.kaggle.com/arnavkj95/data-science-bowl-2017/candidate-generation-and-luna16-preprocessing/notebook
 
-
 import os
+import time
+import pickle
+
 from skimage.morphology import ball, binary_closing
-from skimage.measure import label,regionprops
+from skimage.measure import label, regionprops
 from skimage import measure
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-import dicom
-import pickle
 
 import sys
 sys.path.append("../")
 # Import our competition variables
 from competition_config import *
-d=nodules_roi_dashboard
+D5 = nodules_roi_dashboard
 
-import time
-start_time = time.time()
+START_TIME = time.time()
 
-INPUT_DIRECTORY = d['INPUT_DIRECTORY']
-OUTPUT_DIRECTORY = d['OUTPUT_DIRECTORY']
+if not os.path.exists(D5['OUTPUT_DIRECTORY']):
+    os.makedirs(D5['OUTPUT_DIRECTORY'])
 
-if not os.path.exists(OUTPUT_DIRECTORY):
-    os.makedirs(OUTPUT_DIRECTORY)
-
-# Input data files are available in the INPUT_DIRECTORY.
-# Any results you write has to be saved in the OUTPUT_DIRECTORY.
+# Input data files are available in the D5['INPUT_DIRECTORY'].
+# Any results you write has to be saved in the D5['OUTPUT_DIRECTORY'].
 
 
 def plot_ct_scan(scan):
     f, plots = plt.subplots(int(scan.shape[0] / 20) + 1, 4, figsize=(25, 25))
     for i in range(0, scan.shape[0], 5):
         plots[int(i / 20), int((i % 20) / 5)].axis('off')
-        plots[int(i / 20), int((i % 20) / 5)].imshow(scan[i], cmap=plt.cm.bone) 
+        plots[int(i / 20), int((i % 20) / 5)].imshow(scan[i], cmap=plt.cm.bone)
 
 
 def plot_3d(image, threshold=-300):
-    
-    # Position the scan upright, 
+
+    # Position the scan upright,
     # so the head of the patient would be at the top facing the camera
-    p = image.transpose(2,1,0)
-    p = p[:,:,::-1]
-    
+    p = image.transpose(2, 1, 0)
+    p = p[:, :, ::-1]
+
     verts, faces = measure.marching_cubes(p, threshold)
 
     fig = plt.figure(figsize=(10, 10))
@@ -60,10 +63,10 @@ def plot_3d(image, threshold=-300):
     ax.set_zlim(0, p.shape[2])
 
     plt.show()
-    
+
 
 def get_nodules(segmented_ct_scan):
-    
+
     segmented_ct_scan[segmented_ct_scan < 604] = 0
     #plot_ct_scan(segmented_ct_scan)
     #plot_3d(segmented_ct_scan, 604)
@@ -71,7 +74,7 @@ def get_nodules(segmented_ct_scan):
     # After filtering, there are still lot of noise because of blood vessels.
     # Thus we further remove the two largest connected component.
 
-    selem = ball(d['BALL_RADIUS'])
+    selem = ball(D5['BALL_RADIUS'])
     binary = binary_closing(segmented_ct_scan, selem)
 
     label_scan = label(binary)
@@ -82,31 +85,31 @@ def get_nodules(segmented_ct_scan):
     for r in regionprops(label_scan):
         max_x, max_y, max_z = 0, 0, 0
         min_x, min_y, min_z = 1000, 1000, 1000
-        
+
         for c in r.coords:
             max_z = max(c[0], max_z)
             max_y = max(c[1], max_y)
             max_x = max(c[2], max_x)
-            
+
             min_z = min(c[0], min_z)
             min_y = min(c[1], min_y)
             min_x = min(c[2], min_x)
-        if (min_z == max_z or min_y == max_y or min_x == max_x or r.area > areas[-3]):
+        if min_z == max_z or min_y == max_y or min_x == max_x or r.area > areas[-3]:
             for c in r.coords:
                 segmented_ct_scan[c[0], c[1], c[2]] = 0
         else:
             index = (max((max_x - min_x), (max_y - min_y), (max_z - min_z))) / \
-                 (min((max_x - min_x), (max_y - min_y) , (max_z - min_z)))
-    
-    '''Comment from ArnavJain on Kaggle about index: 
+                 (min((max_x - min_x), (max_y - min_y), (max_z - min_z)))
+
+    '''Comment from ArnavJain on Kaggle about index:
     Index is the shape index of the 3D blob. As mentioned in the TODOs, 
     I am working on reducing the generated candidates using shape index 
     and other properties. So, index is one such property.
     I will post the whole part as soon as I complete it.'''
-            
+
     return segmented_ct_scan
 
-        
+
 def get_nodules_all_segmented_ct_scans(input_path, output_path, image): # Iterate through all pickles
     # Initialize lenght of previus batch to not iterate through errors
     patients = []
@@ -128,7 +131,7 @@ def get_nodules_all_segmented_ct_scans(input_path, output_path, image): # Iterat
                 segmented_ct_scan = pickle.load(handle)
 
             # Get nodules
-            print('Getting nodules from' + patient + '...')
+            print('Getting nodules from ' + patient + '...')
             try:
                 segmented_nodules_ct_scan = get_nodules(segmented_ct_scan)
 
@@ -142,7 +145,7 @@ def get_nodules_all_segmented_ct_scans(input_path, output_path, image): # Iterat
 
                 # Save object as a .pickle
                 with open(output_path + patient, 'wb') as handle:
-                    pickle.dump(segmented_nodules_ct_scan, handle, protocol=PICKLE_PROTOCOL)    
+                    pickle.dump(segmented_nodules_ct_scan, handle, protocol=PICKLE_PROTOCOL)
 
                 # Print and time to finish
                 i_time = time.time() - i_start_time
@@ -155,7 +158,7 @@ def get_nodules_all_segmented_ct_scans(input_path, output_path, image): # Iterat
                 print(patient + ' patient rised an IndexError! Continuing...')
 
 # Turn to True to save a 3D segmented nodule image
-get_nodules_all_segmented_ct_scans(INPUT_DIRECTORY, OUTPUT_DIRECTORY, False)
+get_nodules_all_segmented_ct_scans(D5['INPUT_DIRECTORY'], D5['OUTPUT_DIRECTORY'], False)
 
 print("Total elapsed time: " + \
-      str(time.strftime('%H:%M:%S', time.gmtime((time.time() - start_time)))))
+      str(time.strftime('%H:%M:%S', time.gmtime((time.time() - START_TIME)))))
