0a21644cf4d12d98b56cbb77a206e83668568ab0
Adrián <info@adrianmelic.com>
2017-03-05 08:21:39 +0100

    feat: AWS ephemeral HDD test

--- src/1_lungs_roi_arnav.py
+++ src/1_lungs_roi_arnav.py
@@ -18,19 +18,19 @@ from skimage.measure import label, regionprops
 from skimage.filters import roberts
 from skimage.segmentation import clear_border
 from scipy import ndimage as ndi
-if AWS:
-    import boto3
-    s3 = boto3.client('s3')
-    from matplotlib import use as pltuse
-    pltuse('Agg') # Avoid no display name and environment variable error on AWS
-import matplotlib.pyplot as plt
-
 
 import sys
 sys.path.append("../")
 # Import our competition variables
 from competition_config import *
 D1 = arnavs_lugns_roi_dashboard
+
+if AWS:
+    import boto3
+    s3 = boto3.client('s3')
+    from matplotlib import use as pltuse
+    pltuse('Agg') # Avoid no display name and environment variable error on AWS
+import matplotlib.pyplot as plt
 
 
 START_TIME = time.time()
@@ -140,7 +140,7 @@ def segment_all_ct_scans(input_path, output_path, image): # Iterate through all 
     while True:
         # batch_to_process function from competition_config
         patients, len_prev_batch = \
-        batch_to_process(input_path, output_path, True), len(patients)
+        batch_to_process(input_path, output_path, AWS, True), len(patients)
         if len(patients) == 0 or len(patients) == len_prev_batch:
             break
 
@@ -151,13 +151,17 @@ def segment_all_ct_scans(input_path, output_path, image): # Iterate through all 
                 print(patient + ' already processed. Skipping...')
                 continue
 
-            # Load pickle with resized ct_scan
-            with open(input_path + patient, 'rb') as handle:
-                ct_scan = pickle.load(handle)
-
             # Segment
             print('Segmenting ' + patient + '...')
             try:
+                if AWS:
+                    # Bucket download to the INPUT_DIRECTORY without '../'
+                    s3.download_file(BUCKET, input_path[3:] + patient, \
+                                     input_path + patient)
+                # Load pickle with resized ct_scan
+                with open(input_path + patient, 'rb') as handle:
+                    ct_scan = pickle.load(handle)
+                
                 segmented_ct_scan = segment_lung_from_ct_scan(ct_scan)
 
                 # If true, save image as .png with input image and binary mask superimposed
@@ -170,7 +174,9 @@ def segment_all_ct_scans(input_path, output_path, image): # Iterate through all 
                         print('Uploading to S3', patient_img)
                         # Bucket upload to the OUTPUT_DIRECTORY without '../'
                         s3.upload_file(output_path + patient_img, \
-                                       BUCKET, D1['OUTPUT_DIRECTORY'][3:] + patient_img)
+                                       BUCKET, output_path[3:] + patient_img)
+                        # Delete file to keep hard drive clean and ephemeral
+                        os.remove(output_path + patient_img)
                     plt.close()
 
                 # Save object as a .pickle
@@ -180,7 +186,9 @@ def segment_all_ct_scans(input_path, output_path, image): # Iterate through all 
                         print('Uploading to S3', patient)
                         # Bucket upload to the OUTPUT_DIRECTORY without '../'
                         s3.upload_file(output_path + patient, \
-                                       BUCKET, D1['OUTPUT_DIRECTORY'][3:] + patient)
+                                       BUCKET, output_path[3:] + patient)
+                        # Delete file to keep hard drive clean and ephemeral
+                        os.remove(output_path + patient)
 
                 # Print and time to finish
                 i_time = time.time() - i_start_time
