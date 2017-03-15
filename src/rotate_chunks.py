52debcc739af312e7defedd35faa2268ec358b81
Adrián Melic <info@adrianmelic.com>
2017-03-15 04:09:25 +0100

    feat: Add show option to plot rotation, default False

--- src/rotate_chunks.py
+++ src/rotate_chunks.py
@@ -30,42 +30,41 @@ def save_chunk(chunk, name):
     with open(output, 'wb') as handle:
         pickle.dump(chunk, handle, protocol=PICKLE_PROTOCOL)
 
-def rotate90(chunk, name, axis):
+def rotate90(chunk, name, axis, show=False):
     '''Will rotate chunk 90º clockwise 0, 1, 2 and 3 times for each wall.
     Rotations are like head to left shoulder'''
     rotations = ['0', '90', '180', '270']
     print('rotate90', name)
     for rot in range(4):
-
         rot_chunk = np.rot90(chunk, rot, axis)
         save_chunk(rot_chunk, name+[rotations[rot]])
-        plt_chunk(rot_chunk)
+        if show: plt_chunk(rot_chunk)
         print('Rotation', rotations[rot])
 
-def rotate_chunk(chunk, name):
+def rotate_chunk(chunk, name, show=False):
     '''If you were lyin in a bed, you would rotate 4 times facing each
     wall of the room, floor and celling. Rotations are like head to left shoulder
     z = head to feet, x = left to right, y = back to chest'''
 
     # Patient is lying looking at the celling 'y' (original position)
     # Rotate bed clockwise
-    rotate90(chunk, name+['y'], (0,2))
+    rotate90(chunk, name+['y'], (0,2), show)
     # Turn upside down looking at the floor '-y' and rotate bed clockwise
-    rotate90(np.rot90(chunk, 2, (2,1)), name+['-y'], (2,0))
+    rotate90(np.rot90(chunk, 2, (2,1)), name+['-y'], (2,0), show)
     # Patient get out of bed on the right side of bed, standing up
     chunk = np.rot90(chunk, 1, (1,0)) # rises
     chunk = np.rot90(chunk, 1, (0,2)) # now right side of bed
     # Patient does a cart-wheel!
-    rotate90(chunk, name+['x'], (1,0))
+    rotate90(chunk, name+['x'], (1,0), show)
     # Turns around and another cart-wheel
-    rotate90(np.rot90(chunk, 2, (2,0)), name+['-x'], (0,1))
+    rotate90(np.rot90(chunk, 2, (2,0)), name+['-x'], (0,1), show)
     # Patient, standing up on the right of the bed, now faces the end of bed
     # The bed can be touched by his/her left hand
     chunk = np.rot90(chunk, 1, (2,0))
     # Cart-wheeh! (removing the bed so it does not interfere)
-    rotate90(chunk, name+['z'], (2,1))
+    rotate90(chunk, name+['z'], (2,1), show)
     # Turns around and another cart-wheel
-    rotate90(np.rot90(chunk, 2, (2,0)), name+['-z'], (1,2))
+    rotate90(np.rot90(chunk, 2, (2,0)), name+['-z'], (1,2), show)
 
 def rotate_all_chunks(input_path, output_path):
     '''Rotate all input directory chunks and save them on output directory'''
@@ -86,8 +85,8 @@ def rotate_all_chunks(input_path, output_path):
 
 if __name__ == "__main__":
     START_TIME = time.time()
-
-    rotate_all_chunks(augment['INPUT_DIRECTORY'], augment['OUTPUT_DIRECTORY'])
+    # Turn show to True to plot rotations
+    rotate_all_chunks(augment['INPUT_DIRECTORY'], augment['OUTPUT_DIRECTORY'], show=False)
 
     print('Total elapsed time: ' + \
           str(time.strftime('%H:%M:%S', time.gmtime((time.time() - START_TIME)))))
